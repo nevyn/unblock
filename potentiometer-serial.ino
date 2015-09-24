74c7218c3d1ffc4429140576926809c777d012f3
Nevyn Bengtsson <nevyn.jpg@gmail.com>
2015-09-24 11:01:49 -0700

    arduino code

---
+++ potentiometer-serial.ino
@@ -0,0 +1,13 @@
+void setup() {
+  Serial.begin(38400);
+}
+
+void loop() {
+  int val1 = analogRead(0);
+  int val2 = analogRead(1);
+  Serial.print(val1);
+  Serial.print(" ");
+  Serial.print(val2);
+  Serial.println("");
+  delay(200);
+}
